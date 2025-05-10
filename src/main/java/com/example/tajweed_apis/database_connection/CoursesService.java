96b2c464f92f6b845bd0320b464ec80097f7d633
Salah Tawafsha <salaht321@gmail.com>
2025-05-10 23:40:28 +0300

    Support get teachers in dashboard

--- src/main/java/com/example/tajweed_apis/database_connection/CoursesService.java
+++ src/main/java/com/example/tajweed_apis/database_connection/CoursesService.java
@@ -12,38 +12,50 @@ public class CoursesService {
         Statement statement = connection.createStatement();
 
         ResultSet resultSet = statement.executeQuery("""
-            SELECT
-                c.id,
-                c.level_id,
-                t.name as teacher_name,
-                COALESCE(student_counts.students_count, 0) as students_count,
-                COALESCE(course_times.times, '[]') as course_times
-            FROM
-                course c
-                JOIN teacher t ON c.teacher_id = t.id
-                LEFT JOIN (
-                    SELECT
-                        course_id,
-                        COUNT(*) AS students_count
-                    FROM student_course
-                    GROUP BY course_id
-                ) AS student_counts ON student_counts.course_id = c.id
-                LEFT JOIN (
-                    SELECT
-                        course_id,
-                        JSON_ARRAYAGG(JSON_OBJECT('day', day,'time', time)) AS times
-                    FROM course_time
-                    GROUP BY course_id
-                ) AS course_times ON course_times.course_id = c.id
-            WHERE
-                c.status = 'تعمل'
-            order by
-            	JSON_UNQUOTE(course_times.times);
+            SELECT ct.day,
+                   JSON_ARRAYAGG(
+                       JSON_OBJECT(
+                           'course_id', c.id,
+                           'level_id', c.level_id,
+                           'day', ct.day,
+                           'time', ct.time,
+                           'teacher_name', t.name,
+                           'students_count', COALESCE(student_counts.students_count, 0)
+                       )
+                   ) as courses_list
+            FROM course c
+                     JOIN teacher t ON c.teacher_id = t.id
+                     LEFT JOIN (SELECT course_id,
+                                       COUNT(*) AS students_count
+                                FROM student_course
+                                GROUP BY course_id) AS student_counts ON student_counts.course_id = c.id
+                     LEFT JOIN course_time ct ON ct.course_id = c.id
+            WHERE c.status = 'تعمل'
+            group by ct.day;
         """);
 
         return getResultSetValues(resultSet);
     }
-    public static Map<String, Object> getCourse(String id) throws SQLException {
+    public static List<Map<String, Object>> getTeachers() throws SQLException {
+        Connection connection = DBConnection.getInstance();
+        Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery("""
+            select 
+                t.id, 
+                t.name, 
+                t.year_of_birth, 
+                count(*) as count_of_courses
+            from course c
+                     join teacher t on c.teacher_id = t.id
+            where c.status = "تعمل"
+            group by c.teacher_id
+            order by count_of_courses desc ;
+        """);
+
+        return getResultSetValues(resultSet);
+    }
+    public static Map<String, Object> getCourseDetails(String id) throws SQLException {
         Connection connection = DBConnection.getInstance();
 
         PreparedStatement statement = connection.prepareStatement("""
@@ -51,7 +63,7 @@ public class CoursesService {
             	distinct c.id,
             	t.name as teacher_name,
             	c.level_id,
-            	JSON_ARRAYAGG(JSON_OBJECT('student_id', s.id ,'student_name', s.name, 'year_of_birth', s.year_of_birth)) over (partition by c.id) as students,
+            	JSON_ARRAYAGG(JSON_OBJECT('student_id', s.id ,'student_name', s.name, 'year_of_birth', s.year_of_birth)) over (partition by c.id order by c.id) as students,
             	JSON_ARRAYAGG(JSON_OBJECT('time', ct.`time` ,'day', ct.`day` , 'place', ct.place)) over (partition by c.id, s.id, ct.id) as times,
             	JSON_ARRAYAGG(JSON_OBJECT('note', cn.note  ,'date', cn.`date`)) over (partition by c.id, s.id, ct.id) as notes
             from
@@ -60,12 +72,12 @@ public class CoursesService {
             	c.id = sc.course_id
             join student s on
             	sc.student_id = s.id
-            join teacher t on\s
+            join teacher t on
             	c.teacher_id = t.id
-            left join course_time ct on\s
-            	c.id  = ct.course_id\s
+            left join course_time ct on
+            	c.id  = ct.course_id
             left join course_note cn on
-            	c.id = cn.course_id\s
+            	c.id = cn.course_id
             where c.id = ?;
         """);
 
--- src/main/java/com/example/tajweed_apis/rest_apis/CourseAPIs.java
+++ src/main/java/com/example/tajweed_apis/rest_apis/CourseAPIs.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,11 +17,16 @@ import java.util.Map;
 @CrossOrigin(origins = "http://localhost:4200") // Replace with your Angular app's URL
 public class CourseAPIs {
 
-    @GetMapping("/courses")
-    public ResponseEntity<String> getCourses() {
+    @GetMapping("/dashboard")
+    public ResponseEntity<String> getDashboardInfo() {
         try {
             List<Map<String, Object>> courses = CoursesService.getCourses();
+            List<Map<String, Object>> teachers = CoursesService.getTeachers();
             Gson gson = new Gson();
+
+            Map<String, List<Map<String, Object>>> response = new HashMap<>();
+            response.put("courses", courses);
+            response.put("teachers", teachers);
 
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
@@ -28,7 +34,7 @@ public class CourseAPIs {
             return ResponseEntity
                     .ok()
                     .headers(headers)
-                    .body(gson.toJson(courses));
+                    .body(gson.toJson(response));
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -36,7 +42,7 @@ public class CourseAPIs {
     @GetMapping("/courses/{id}")
     public ResponseEntity<String> getCourseDetails(@PathVariable String id) {
         try {
-            Map<String, Object> courseDetails = CoursesService.getCourse(id);
+            Map<String, Object> courseDetails = CoursesService.getCourseDetails(id);
             Gson gson = new Gson();
 
             HttpHeaders headers = new HttpHeaders();
